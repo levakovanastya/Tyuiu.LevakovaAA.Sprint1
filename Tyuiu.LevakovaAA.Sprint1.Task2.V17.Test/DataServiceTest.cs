d03f720a7947d2f4562fe5ea85fb0aafb8af34bf
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-09-29 18:49:33 +0500

    Task2

---
+++ Tyuiu.LevakovaAA.Sprint1.Task2.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LevakovaAA.Sprint1.Task2.V17.Lib;
+namespace Tyuiu.LevakovaAA.Sprint1.Task2.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 90;
+            var res = ds.ConvertMinutesToHours(x);
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
